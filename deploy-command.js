7a065123b412c9924e17c2f0df4e54e8e734aaed
Nicholas Tran <nicholas.ac.tran@gmail.com>
2022-09-15 02:33:09 -0400

    Initial commit

---
+++ deploy-command.js
@@ -0,0 +1,21 @@
+// https://discordjs.guide/creating-your-bot/creating-commands.html#replying-to-commands
+// Deployment script for commands
+
+require('dotenv').config(); //initialize dotenv
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+
+const token = process.env.CLIENT_TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD;
+
+const commands = [
+    new SlashCommandBuilder().setName('dailyjazzmove').setDescription('Gives you a daily jazz move to practice!'),
+    new SlashCommandBuilder().setName('jazzcombo').setDescription('Gives you a daily jazz combo to practice!')
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
---
+++ index.js
@@ -0,0 +1,77 @@
+require('dotenv').config(); //initialize dotenv
+
+const { 
+    Client, 
+    Intents, 
+    ThreadAutoArchiveDuration,
+} = require('discord.js');
+const cron = require('cron');
+const moment = require('moment');
+
+const Cache = require('./lib/cache.js');
+const jazzCommands = require('./lib/jazzCommands');
+
+const token = process.env.CLIENT_TOKEN;
+const channelId = process.env.CHANNEL;
+
+let cache;
+
+const client = new Client({
+    intents: []
+}); //create new client
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  cache = new Cache();
+
+  // Cron job for 20:00 every Wednesday
+  // Creates thread in practice channel
+  const scheduledWeeklyThread = new cron.CronJob('00 00 20 * * WED', async () => {
+      const channel = await client.channels.fetch(channelId);
+      const date = moment().format('YYYY-MM-DD');
+      
+      const thread = await channel.threads.create({
+          name: `Wednesday Weekly Whatcha Working On? ${date}`,
+          autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
+          reason: "Keep yourself accountable"
+      }).catch(console.error);
+
+      await thread.send("Share whatcha've been working on! <a:ablobwobwork:1002344417040015421>");
+  });
+
+  scheduledWeeklyThread.start();
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'dailyjazzmove') {
+        const date = moment().format('YYYY-MM-DD');
+        const jazzCache = cache.getCache().dailyJazz;
+        
+        if (jazzCache.hasOwnProperty(date)) {
+            await interaction.reply(jazzCache[date]);
+        } else {
+            const jazzMove = jazzCommands.jazzMove();
+            cache.writeDailyJazz(date, jazzMove);
+            await interaction.reply(jazzMove);
+        }
+    } else if (commandName === 'jazzcombo') {
+        const date = moment().format('YYYY-MM-DD');
+        const comboCache = cache.getCache().jazzCombo;
+        
+        if (comboCache.hasOwnProperty(date)) {
+            await interaction.reply(comboCache[date]);
+        } else {
+            const jazzCombo = jazzCommands.jazzCombo();
+            cache.writeJazzCombo(date, jazzCombo);
+            await interaction.reply(jazzCombo);
+        }
+    }
+})
+
+//make sure this line is the last line
+client.login(token); //login bot using token
